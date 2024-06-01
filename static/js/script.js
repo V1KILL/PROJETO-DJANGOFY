5ee780611739313d09fb3d115315cabe8fc2e3fa
Victor Amaral Mendonça <canalv1kill@gmail.com>
2024-06-01 18:53:56 -0300

    Done Home Page

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,12 @@
-const swiper = new Swiper('.swiper', 
-    // Optional parameters
-    
-    loop: true,
-    slidesPerView: 2,
-    spaceBetween: 16,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
+var perfil = document.querySelector('.perfil');
+var options = document.querySelector('.perfil-image .options');
 
-document.getElementById('btn-pagar').addEventListener('click', function() {
-  // Lógica para chamar a view de processamento de pagamento via AJAX
-  fetch('/pagamento')
-      .then(response => response.json())
-      .then(data => {
-          if (data.url_pagamento) {
-              // Redireciona o usuário para a página de pagamento do PagSeguro
-              window.location.href = data.url_pagamento;
-          } else {
-              alert('Erro ao processar pagamento: ' + data.erro);
-          }
-      })
-      .catch(error => {
-          console.error('Erro ao processar pagamento:', error);
-      });
+perfil.addEventListener('click', function() {
+
+  if (options.style.display === 'flex') {
+    options.style.display = 'none';
+  } else {
+    options.style.display = 'flex';
+  }
+  
 });
