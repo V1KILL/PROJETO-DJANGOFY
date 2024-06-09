335938df28fe79b7bdae64fb5ebb2b3dcbdd0d34
Victor Amaral Mendonça <canalv1kill@gmail.com>
2024-06-09 01:23:31 -0300

    Registrando os Modelos no Admin

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import Video, Module, Topic, Comment, UserProfile
+
+admin.site.register(Video)
+admin.site.register(Module)
+admin.site.register(Topic)
+admin.site.register(Comment)
+admin.site.register(UserProfile)
+
+
--- courseapp/models.py
+++ courseapp/models.py
@@ -3,9 +3,16 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profileimg = models.ImageField(upload_to='profiles/', default='1708190589974.jpg')
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
 class Topic(models.Model):
     title = models.CharField(max_length=200)
-    module = models.ForeignKey('Module', on_delete=models.CASCADE, related_name='topics')
 
     def __str__(self):
         return self.title
@@ -17,6 +24,7 @@ class Module(models.Model):
     status = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
     classes = models.IntegerField(default=0)
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE, related_name='topics')
 
     def __str__(self):
         return self.title
